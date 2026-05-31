2189275f9abb929eb3a3b36e245eb7fbdfb2456f
inderjeet <inderjeetsinghkhalsa445@gmail.com>
2026-05-31 12:44:51 +0530

    adding new folder

---
+++ project5/script.js
@@ -0,0 +1,10 @@
+var maths=Number(prompt("enter 1 no"))
+var physics=Number(prompt("enter 2 no"))
+var c=Number(prompt("enter 3 no"))
+if(maths+physics+c/3>=85)
+{
+    alert("pass ho gya mitr")
+}
+else{
+    alert("fail h bhai padh lee")
+}
